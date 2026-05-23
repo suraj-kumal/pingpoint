d202bd923669a9136729ec62d5cd15153669de3f
Suraj Kumal <surajkumal8773@gmail.com>
2026-05-23 16:16:16 +0545

    add : login form

---
+++ pages/admin/login.vue
@@ -0,0 +1,10 @@
+<template>
+    <h1>login</h1>
+
+    <form>
+        <input type="email" placeholder="Email" required />
+        <input type="password" placeholder="Password" required />
+    </form>
+</template>
+
+<script></script>
